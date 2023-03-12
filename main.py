c27000a5e832a130011509d1b19949362d8f648e
Steve Rector <steverector27@gmail.com>
2023-03-12 13:39:59 -0700

    Refactor tail collision, complete mvp

--- main.py
+++ main.py
@@ -44,11 +44,9 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
 
-    # Detect collision with tail?
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
 
 scoreboard.game_over()
